7fd28d787c86421b3fceb96c8485b08583020202
Daniele Simas <83620485+danisimas@users.noreply.github.com>
2023-02-16 16:46:31 -0400

    códigos novos

--- teste1/client1.py
+++ teste1/client1.py
@@ -1,16 +1,105 @@
 import socket
+import threading
+from Crypto.Cipher import ARC4
+from Crypto.Util.number import getPrime, getRandomRange
 
-HOST = 'localhost'  # Endereço IP do servidor
-PORT = 5000  # Porta em que o servidor está escutando
+host = 'localhost'
+port = 5555
+# Gera um número primo aleatório com até 25 dígitos
+def generate_prime():
+    return getPrime(25)
 
+# Gera um valor aleatório para o Diffie-Hellman
+def generate_dh_value():
+    return getRandomRange(2, generate_prime())
+
+# Implementa o protocolo de Diffie-Hellman
+def diffie_hellman(conn):
+    # Gera o valor secreto do cliente
+    client_secret = generate_dh_value()
+    
+    # Calcula o valor público do cliente
+    client_public = pow(5, client_secret, generate_prime())
+    
+    # Envia o valor público do cliente para o servidor
+    conn.send(str(client_public).encode("ISO-8859-1"))
+    
+    # Recebe o valor público do servidor
+    server_public = int(conn.recv(1024).decode("ISO-8859-1"))
+    
+    # Calcula a chave de sessão
+    session_key = pow(server_public, client_secret, generate_prime())
+    
+    return session_key
+
+# Função para receber mensagens do servidor
+def receive_loop(conn, cipher):
+    while True:
+        # Recebe a mensagem cifrada do servidor
+        ciphertext = conn.recv(1024)
+        if not ciphertext:
+            break
+        
+        # Decifra a mensagem com o objeto de criptografia RC4
+        plaintext = cipher.decrypt(ciphertext).decode("ISO-8859-1")
+        
+        print(plaintext)
+
+# Função para enviar mensagens para o servidor
+def send_loop(conn, cipher, client_name):
+    while True:
+        # Lê uma mensagem do usuário
+        message = input(f"{client_name}: ")
+        
+        # Cifra a mensagem com o objeto de criptografia RC4
+        ciphertext = cipher.encrypt(message.encode("ISO-8859-1"))
+        
+        # Envia a mensagem cifrada para o servidor
+        conn.send(ciphertext)
+        
+        if message == "exit":
+            break
+
+# Função principal do cliente
 def main():
-    username = input('Digite seu nome de usuário: ')
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.send(username.encode())
-        while True:
-            message = input('> ')
-            s.send(message.encode())
+        # Criando o socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Conectando ao servidor
+    client_socket.connect((host, port))
+    print("Conectado ao servidor.")
+
+    # Enviando nome do cliente ao servidor
+    client_name = input("Digite seu nome: ")
+    client_socket.send(client_name.encode("ISO-8859-1"))
+
+    # Iniciando Diffie-Hellman e obtendo chave de sessão
+    session_key = diffie_hellman(client_socket)
+
+    # Iniciando thread para receber mensagens do servidor
+    receive_thread = threading.Thread(target=receive_loop, args=(client_socket,))
+    receive_thread.start()
+
+    # Loop principal do cliente
+    while True:
+        # Lendo mensagem do usuário
+        message = input("Digite sua mensagem: ")
+
+        # Encriptando e enviando mensagem ao servidor
+        cipher = ARC4.new(session_key)
+        ciphertext = cipher.encrypt(message.encode("ISO-8859-1"))
+        client_socket.send(ciphertext)
+
+        # Aguardando confirmação de recebimento do servidor
+        received = client_socket.recv(1024)
+        if received != b'ACK':
+            print("Erro ao enviar mensagem.")
+        
+        # Inicia as threads de recepção e envio de mensagens
+        receive_thread = threading.Thread(target=receive_loop, args=(client_socket, cipher))
+        send_thread = threading.Thread(target=send_loop, args=(client_socket, cipher, client_name))
+        receive_thread.start()
+        send_thread.start()
 
 if __name__ == "__main__":
     main()
--- teste1/server1.py
+++ teste1/server1.py
@@ -2,122 +2,82 @@ import socket
 import threading
 from Crypto.Cipher import ARC4
 from Crypto.Util.number import getPrime
-from Crypto.Random import get_random_bytes
+from base64 import b64encode, b64decode
 
-HOST = 'localhost'  # Endereço do servidor
-PORT = 5000  # Porta do servidor
-PRIME = getPrime(128)  # Número primo usado no Diffie-Hellman
-GENERATOR = 2  # Gerador usado no Diffie-Hellman
+# Lista de conexões de clientes
+client_conns = []
+host = '127.0.0.1'
+port = 5555
+def handle_client(conn, addr):
+    # Cria um novo objeto de criptografia RC4 com uma chave aleatória
+    key = getPrime(25)
+    cipher = ARC4.new(str(key).encode())
 
-class ChatRoom:
-    def _init_(self):
-        self.users = {}  # Dicionário que mapeia o nome do usuário para o objeto de conexão
-        self.lock = threading.Lock()  # Lock para evitar condições de corrida
+    # Recebe o nome do cliente
+    client_name = conn.recv(1024).decode()
+    print(f"[INFO] {client_name} conectado com sucesso")
 
-    def broadcast(self, sender, message):
-        """
-        Envia uma mensagem para todos os usuários, exceto o remetente.
-        """
-        encrypted_message = self.encrypt_message(sender, message)
-        for username, conn in self.users.items():
-            if username != sender:
-                conn.send(encrypted_message)
+    # Adiciona a conexão do cliente à lista de conexões
+    client_conns.append(conn)
 
-    def handle(self, conn, addr):
-        """
-        Lida com uma conexão de usuário.
-        """
-        # Inicia o protocolo Diffie-Hellman
-        conn.send(f'{PRIME},{GENERATOR}'.encode())
-        client_public_key = int(conn.recv(1024).decode())
-        secret = pow(client_public_key, get_random_bytes(1)[0], PRIME)
+    try:
+        while True:
+            # Recebe a mensagem cifrada do cliente
+            ciphertext = conn.recv(1024)
+            if not ciphertext:
+                break
 
-        # Recebe o nome do usuário
-        username = conn.recv(1024).decode()
-        print(f'{username} se conectou de {addr}')
+            # Decifra a mensagem com o objeto de criptografia RC4
+            plaintext = cipher.decrypt(ciphertext).decode("ISO-8859-1")
 
-        # Adiciona o usuário à lista de usuários
-        with self.lock:
-            self.users[username] = conn
+            # Adiciona o nome do cliente à mensagem recebida e a codifica em UTF-8
+            message = f"[{client_name}] {plaintext}"
+            encoded_message = message.encode("ISO-8859-1")
 
-        try:
-            while True:
-                # Recebe uma mensagem do usuário
-                encrypted_message = conn.recv(1024)
-                if not encrypted_message:
-                    break
-                message = self.decrypt_message(username, encrypted_message)
-                print(f'{username}: {message}')
+            # Cifra a mensagem com o objeto de criptografia RC4
+            ciphertext = cipher.encrypt(encoded_message)
 
-                # Envia a mensagem para todos os usuários
-                self.broadcast(username, message)
-
-                # Envia uma nova chave de criptografia
-                conn.send(get_random_bytes(1) + secret.to_bytes(16, 'big'))
-        except:
-            # Remove o usuário da lista de usuários se a conexão for encerrada
-            with self.lock:
-                del self.users[username]
-            print(f'{username} desconectou')
-            self.broadcast(username, f'{username} saiu do chat.')
-
-    def encrypt_message(self, sender, message):
-        """
-        Criptografa uma mensagem usando RC4 com a chave secreta compartilhada entre o remetente e o receptor.
-        """
-        key = self.get_secret_key(sender)
-        cipher = ARC4.new(key)
-        return cipher.encrypt(message.encode())
-
-    def decrypt_message(self, receiver, encrypted_message):
-        """
-        Descriptografa uma mensagem usando RC4 com a chave secreta compartilhada entre o remetente e o receptor.
-        """
-        key = self.get_secret_key(receiver)
-        cipher = ARC4.new(key)
-        return cipher.decrypt(encrypted_message).decode()
-
-    def get_secret_key(self, username):
-      """
-      Retorna a chave secreta compartilhada entre o usuário especificado e o remetente da última mensagem.
-      """
-      with self.lock:
-          usernames = list(self.users.keys())
-      index = usernames.index(username)
-      sender = usernames[index - 1]  # Remetente da última mensagem
-      secret = self.get_secret(sender, username)
-      return secret.to_bytes(16, 'big')
-
-    def get_secret(self, user1, user2):
-        """
-        Calcula a chave secreta compartilhada entre dois usuários usando o protocolo Diffie-Hellman.
-        """
-        private_key = get_random_bytes(1)[0]
-        client_public_key = pow(GENERATOR, private_key, PRIME)
-        self.users[user1].send(str(client_public_key).encode())
-        server_public_key = int(self.users[user1].recv(1024).decode())
-        secret = pow(server_public_key, private_key, PRIME)
-
-        private_key = get_random_bytes(1)[0]
-        client_public_key = pow(GENERATOR, private_key, PRIME)
-        self.users[user2].send(str(client_public_key).encode())
-        server_public_key = int(self.users[user2].recv(1024).decode())
-        secret2 = pow(server_public_key, private_key, PRIME)
-
-        return secret
-    
-    def start(self):
-        """
-        Inicia o servidor e espera por conexões de usuários.
-        """
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            print(f'O servidor está ouvindo em {HOST}:{PORT}')
-            while True:
-                conn, addr = s.accept()
-                threading.Thread(target=self.handle, args=(conn, addr)).start()
+            # Envia a mensagem cifrada para todos os clientes conectados
+            for client_conn in client_conns:
+                client_conn.send(ciphertext)
+    finally:
+        # Remove a conexão do cliente da lista de conexões
+        client_conns.remove(conn)
+        conn.close()
+        print(f"[INFO] {client_name} desconectado")
 
 if __name__ == "__main__":
-    chatroom = ChatRoom()
-    chatroom.start()
+    # Criando o socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Ligando o socket a um endereço e porta
+    server_socket.bind((host, port))
+
+    # Iniciando o servidor
+    server_socket.listen()
+    print(f"Servidor iniciado no endereço {host}:{port}")
+
+# Loop principal do servidor
+while True:
+    # Aceitando conexões de clientes
+    conn, addr = server_socket.accept()
+    print(f"Novo cliente conectado: {addr[0]}:{addr[1]}")
+
+    # Criando thread para lidar com o cliente
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+
+    print("[INFO] Servidor iniciado na porta 5555")
+
+    try:
+        while True:
+            # Aceita uma nova conexão
+            conn, addr = server_socket.accept()
+
+            # Cria uma nova thread para lidar com o cliente
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+            client_thread.start()
+    finally:
+        # Fecha o socket do servidor
+        server_socket.close()
